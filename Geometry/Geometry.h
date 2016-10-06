77fff2ab8a4f0203282eb69780bd2152d13eb730
Vasiliy <kaktusv6@gmail.com>
2016-10-07 01:28:22 +1000

    Добавил общий файл Geometry для всех классов.

---
+++ Geometry/Geometry.h
@@ -0,0 +1,17 @@
+//
+// Created by vasiliy on 05.10.16.
+//
+
+#ifndef LAB1BASEGEOMETRY_GEOMETRY_H
+#define LAB1BASEGEOMETRY_GEOMETRY_H
+
+
+
+template<int Size>
+class Vector
+{
+	double components[Size];
+};
+
+
+#endif //LAB1BASEGEOMETRY_GEOMETRY_H
