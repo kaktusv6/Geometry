004b7e7dab82f7e2aa1eddfa22802a94cc67a6dd
Vasiliy <kaktusv6@gmail.com>
2016-10-09 23:44:58 +1000

    Добавил клаас ExeptionOutRang ошибок выхода за массив. Определил оператор [].

--- Geometry/Vector.h
+++ Geometry/Vector.h
@@ -5,13 +5,14 @@
 #ifndef LAB1BASEGEOMETRY_VECTOR_H
 #define LAB1BASEGEOMETRY_VECTOR_H
 
+#include <iostream>
 #include <cmath>
 
 template<int Size>
 double operator* (Vector<Size>, Vector<Size>);
 template<int Size>
 class Vector {
-	double cmoponents[Size];
+	double components[Size];
 public:
 	Vector();
 	Vector(double);
@@ -20,6 +21,14 @@ public:
 
 	double angle();
 	double length();
+
+	class ExeptionOutOfRang {
+	public:
+		ExeptionOutOfRang() {}
+		void why() {
+			std::cout << "Out of range" << std::endl;
+		}
+	};
 };
 
 template<int Size>
@@ -32,8 +41,22 @@ double operator* (Vector<Size> a, Vector<Size> b) {
 }
 
 template<int Size>
+double Vector<Size>::operator[] (int index) {
+	try{
+		if (index < 0 || Size <= index) {
+			throw ExeptionOutOfRang();
+		}
+	}
+	catch(ExeptionOutOfRang e) {
+		e.why();
+	}
+
+	return components[index];
+}
+
+template<int Size>
 double Vector<Size>::angle() {
-	return std::acos(cmoponents[0] / std::sqrt((*this) * (*this)));
+	return std::acos(components[0] / std::sqrt((*this) * (*this)));
 }
 template<int Size>
 double Vector<Size>::length() {
