4234689a317aa49799397648e74428f46ddbdef2
Vasiliy <kaktusv6@gmail.com>
2016-10-09 23:29:59 +1000

    Написал скалярное умножение между векторами одинаковой размерности.

--- Geometry/Vector.h
+++ Geometry/Vector.h
@@ -11,7 +11,17 @@ class Vector {
 public:
 	Vector();
 	Vector(double);
-	
+
 	double operator[] (int);
 };
+
+template<int Size>
+double operator* (Vector<Size> a, Vector<Size> b) {
+	double s = 0;
+	for(int i = 0; i < Size; i++){
+		s += a[i]*b[i];
+	}
+	return s;
+}
+
 #endif //LAB1BASEGEOMETRY_VECTOR_H
