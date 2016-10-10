96f86f04604d058f1dc1515156b8e429e91e0101
Vasiliy <kaktusv6@gmail.com>
2016-10-10 19:51:13 +1000

    Добавил класс Line.

--- Geometry/Line.h
+++ Geometry/Line.h
@@ -5,4 +5,18 @@
 #ifndef LAB1BASEGEOMETRY_LINE_H
 #define LAB1BASEGEOMETRY_LINE_H
 
+#include "Point.h"
+#include "Vector.h"
+
+template<int Size>
+class Line {
+	Point<Size> point;
+	Vector<Size> vector;
+public:
+	Line() {}
+	Line(Point<Size>, Vector<Size>);
+
+	~Line() {}
+};
+
 #endif //LAB1BASEGEOMETRY_LINE_H
